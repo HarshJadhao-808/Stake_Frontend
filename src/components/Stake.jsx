9a7c56be24f45af8b80f0f0d58294e9c958e9b80
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2026-02-07 14:30:57 +0530

    stake time

--- src/components/Stake.jsx
+++ src/components/Stake.jsx
@@ -1,51 +1,51 @@
-import axios from 'axios'
-import { jwtDecode } from 'jwt-decode'
-import React, { useEffect, useState } from 'react'
-import Hcoin from '/src/assets/Hcoin.svg'
-
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import React, { useEffect, useState } from "react";
+import Hcoin from "/src/assets/Hcoin.svg";
 
 const Stake = () => {
-	const [data,setData] = useState([])
-	const [stakeit,setStakeit] = useState(false)
-	const [stakebutton,setStakebutton] = useState(false)
-	const token =JSON.parse(localStorage.getItem("Token"))
-	const [stakevalue,setStakevalue] = useState({
-		stakeInput:""
-	})
-	let id 
-	if(token){
-		const decoded = jwtDecode(token)
-		id = decoded.id
+	const [data, setData] = useState([]);
+	const [stakeit, setStakeit] = useState(false);
+	const [stakebutton, setStakebutton] = useState(false);
+	const token = JSON.parse(localStorage.getItem("Token"));
+	const [stakevalue, setStakevalue] = useState({
+		stakeInput: "",
+	});
+	let id;
+	if (token) {
+		const decoded = jwtDecode(token);
+		id = decoded.id;
 	}
-
-
 
 	const savestake = (e) => {
-		setStakevalue({...stakevalue,[e.target.name] : e.target.value})
-	}
-	
-	console.log(stakevalue)
-	const getData = async() => {
+		setStakevalue({ ...stakevalue, [e.target.name]: e.target.value });
+	};
+
+	console.log(stakevalue);
+	const getData = async () => {
 		const res = await axios.get(`http://localhost:8888/user/getData/${id}`, {
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
 		});
-	    setData(res.data.user)
-	    // console.log(res.data.user)
-	}
+		setData(res.data.user);
+		// console.log(res.data.user)
+	};
 
-		const SendStake = async() => {
-	const res =await axios.post(`http://localhost:8888/user/stake/${id}`,{stake:Number(stakevalue.stakeInput)},{headers : {Authorization: `Bearer ${token}`}});
-	console.log(res)
-	getData()
-	}
+	const SendStake = async () => {
+		const res = await axios.post(
+			`http://localhost:8888/user/stake/${id}`,
+			{ stake: Number(stakevalue.stakeInput) },
+			{ headers: { Authorization: `Bearer ${token}` } },
+		);
+		console.log(res);
+		getData();
+	};
 
-
-	useEffect(()=>{
-		getData()
-	},[])
-  return (
+	useEffect(() => {
+		getData();
+	}, []);
+	return (
 		<div>
 			<div className="h-17 sm:h-22 bg-black "></div>
 			<div className="h-800 sm:h-[500px] lg:h-[700px] bg-[rgb(0,0,0)] ">
@@ -54,9 +54,9 @@ const Stake = () => {
 						<div className="bg-[rgb(22,22,26)]   shadow-[rgba(0,0,0,0.1)_0px_4px_6px_-1px,rgba(0,0,0,0.06)_0px_2px_4px_-1px] border-[rgba(245,158,11,0.3)] h-[160px] w-[95%] xl:h-full xl:w-80 rounded-2xl  p-5">
 							<div className="flex flex-col gap-4 text-center sm:text-left">
 								<p className="text-gray-500">WALLET BLANCE</p>
-								<div className="flex gap-2 justify-center sm:justify-start">
-									<p className="text-white">{data.wallet}</p>
-									<img src={Hcoin} className="w-4" alt="" />
+								<div className="flex gap-2 justify-center sm:justify-start ">
+									<p className="text-white text-[20px]">{data.wallet}</p>
+									<img src={Hcoin} className="w-5" alt="" />
 								</div>
 								<p className="text-green-700">0.01% from last minute</p>
 							</div>
@@ -65,7 +65,7 @@ const Stake = () => {
 							<div className="flex flex-col gap-4 text-center sm:text-left">
 								<p className="text-gray-500">TOTAL STAKED</p>
 								<div className="flex gap-2 justify-center sm:justify-start">
-									<p className="text-white">{data.stake}</p>
+									<p className="text-white text-[20px]">{data.stake}</p>
 									<img src={Hcoin} className="w-4" alt="" />
 								</div>
 								<p className="text-green-700">Active</p>
@@ -75,7 +75,7 @@ const Stake = () => {
 							<div className="flex flex-col gap-4 text-center sm:text-left">
 								<p className="text-gray-500">REWARD RATE</p>
 								<div className="flex gap-2 justify-center sm:justify-start">
-									<p className="text-white">0.01</p>
+									<p className="text-white text-[20px]">0.01</p>
 									<p className="text-[rgb(245,158,11)] text-[13px] mt-1">
 										% /10 mins
 									</p>
@@ -85,7 +85,7 @@ const Stake = () => {
 						</div>
 					</div>
 					<div className="border- h-60 w-[99%] grid sm:grid-cols-2 ">
-						<div className="bg-[rgb(22,22,26)]   shadow-[rgba(0,0,0,0.1)_0px_4px_6px_-1px,rgba(0,0,0,0.06)_0px_2px_4px_-1px] border-[rgba(245,158,11,0.3)] h-[220px] w-[95%] xl:h-full  rounded-2xl  p-8 ">
+						<div className="bg-[rgb(22,22,26)]   shadow-[rgba(0,0,0,0.1)_0px_4px_6px_-1px,rgba(0,0,0,0.06)_0px_2px_4px_-1px] border-[rgba(245,158,11,0.3)] w-[95%] xl:h-85  rounded-2xl  p-8 ">
 							<div className="flex flex-col gap-4 text-center sm:text-left">
 								<div className="flex  gap-2 justify-center sm:justify-start">
 									<p className="text-white text-[24px] ">Stake</p>
@@ -150,19 +150,54 @@ const Stake = () => {
 								</div>
 								<div
 									onClick={SendStake}
-									className="text-white rounded-[6px] flex h-12 justify-center items-center gap-2 text-[20px] bg-amber-500 hover:shadow-[0_4px_12px_0px_rgba(217,119,6,0.4)] hover:scale-y-110 duration-200 transition-all"
+									className="text-white rounded-[6px] mt-3 flex h-12 justify-center items-center gap-2 text-[20px] bg-amber-500 hover:shadow-[0_4px_12px_0px_rgba(217,119,6,0.4)] hover:scale-y-110 duration-200 transition-all"
 								>
 									<p>+</p>
 									<p>Stake Now</p>
 								</div>
 							</div>
 						</div>
-						<div className="bg-[rgb(22,22,26)]   shadow-[rgba(0,0,0,0.1)_0px_4px_6px_-1px,rgba(0,0,0,0.06)_0px_2px_4px_-1px] border-[rgba(245,158,11,0.3)] h-[220px] w-[95%] xl:h-full  rounded-2xl  p-8"></div>
+						<div className="bg-[rgb(22,22,26)]   shadow-[rgba(0,0,0,0.1)_0px_4px_6px_-1px,rgba(0,0,0,0.06)_0px_2px_4px_-1px] border-[rgba(245,158,11,0.3)]  w-[95%] xl:h-full  rounded-2xl  p-8  ">
+							<div className="flex flex-col gap-4 text-center sm:text-left">
+								<div className="flex  gap-2 justify-center sm:justify-between">
+									<p className="text-white text-[20px] ">Active Stake</p>
+									<p className="text-[rgb(16,185,129)] bg-[rgba(16,185,129,0.1)] p-1 px-3 ">
+										running
+									</p>
+								</div>
+								<div className="border-10 w-45 h-45 flex m-auto flex-col justify-center items-center rounded-[50%] border-[rgb(243,244,246)] border-t-[rgb(255,196,0)] border-r-[rgb(255,196,0)] ">
+										<p className="text-white text-[28px]">42:55</p>
+										<p className="text-[rgb(156,163,175)] ">until next</p>
+									</div>
+								<div>
+									<div className="flex  gap-2 justify-center sm:justify-between">
+										<p className="text-[rgb(156,163,175)] text-[18px] ">
+											Amount Staked
+										</p>
+										<div className="flex gap-2 ">
+											<p className="text-white text-[19px] ">{data.stake}</p>
+											<img src={Hcoin} className="w-5" alt="" />
+										</div>
+									</div>
+									<div className="flex  gap-2 justify-center mt-3 sm:justify-between">
+										<p className="text-[rgb(156,163,175)] text-[18px] ">
+											Est. Next Reward
+										</p>
+										<div className="flex gap-2 ">
+											<p className="text-white text-[19px] ">
+												{(data.stake / 100) * 10}
+											</p>
+											<img src={Hcoin} className="w-5" alt="" />
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	);
-}
+};
 
-export default Stake
+export default Stake;
